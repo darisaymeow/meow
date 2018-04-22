b4ca2c5f86d46b78bc4feebcbbae3a126cdc28d2
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-04-23 02:16:02 +0600

    Add files via upload

---
+++ lab2/ConsoleApp7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp8
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CapitalLetters capitalizer = new CapitalLetters();
+            Console.WriteLine("Введите строку с данными: ");
+            string str = Console.ReadLine();
+            string result = capitalizer.ToCapital(str);
+            Console.ReadKey();
+            Console.WriteLine($"До: {str}");
+            Console.WriteLine($"После: {result}");
+            Console.ReadLine();
+        }
+    }
+}
