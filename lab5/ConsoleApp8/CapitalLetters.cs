eba660db907a5d69eaa0821e4a6369aedb499ca2
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-04-23 02:27:31 +0600

    Add files via upload

---
+++ lab5/ConsoleApp8/CapitalLetters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp8
+{
+    using System.Security.Cryptography.X509Certificates;
+
+    public class CapitalLetters
+    {
+        public string capital(string result)
+        {
+            char[] split = result.ToCharArray();
+            split[0] = Char.ToUpper(split[0]);
+            for (int n = 0; n < split.Length - 1; n++)
+            {
+
+                if (split[n] == ' ')
+                {
+                    split[n + 1] = Char.ToUpper(split[n + 1]);
+
+
+                }
+
+            }
+
+            String result1 = new String(split);
+            return result1;
+
+
+        }
+    }
+}
---
+++ lab5/ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CapitalLetters object1 = new CapitalLetters();
+            Console.WriteLine("Введите строку с данными: "); string str = Console.ReadLine();
+            string result = object1.capital(str);
+            Console.ReadKey();
+            Console.WriteLine("До: {0}", str);
+            Console.WriteLine("После: {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
