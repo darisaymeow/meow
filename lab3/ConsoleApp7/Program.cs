c6e8cc1076a1253bf955837f9d0d71c369945af7
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-04-23 02:19:20 +0600

    Add files via upload

---
+++ lab3/ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Reiteration arreiteration1 = new Reiteration();
+            Console.WriteLine("Введите размерность массива:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[input];
+            Random ran = new Random();
+            for (int i = 0; i < input; i++)
+            {
+                array[i] = ran.Next(-15, 15);
+                Console.Write("{0} ", array[i]);
+
+            }
+            int result = arreiteration1.ArrReiteration(array);
+            Console.WriteLine();
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab3/ConsoleApp7/Reiteration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp10
+{
+    public class Reiteration
+    {
+        public int ArrReiteration(int[] array)
+        {
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dict.ContainsKey(array[i]))
+                {
+                    dict[array[i]]++;
+                }
+                else
+                {
+                    dict.Add(array[i], 1);
+                }
+            }
+
+            int maxCount = 0;
+            int maxValue = 0;
+            foreach (var element in dict)
+            {
+                if (element.Value > maxCount)
+                {
+                    maxValue = element.Key;
+                    maxCount = element.Value;
+                }
+            }
+
+            return maxValue;
+
+        }
+    }
+}
