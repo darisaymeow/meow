a25caf70112ecc981aa930339a3b45289d8c5a17
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-04-23 02:21:42 +0600

    Add files via upload

---
+++ lab4/ConsoleApp7/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Switch arrswitch1 = new Switch();
+            Console.WriteLine("Введите размерность массива:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[input];
+            Random ran = new Random();
+            for (int i = 0; i < input; i++)
+            {
+                array[i] = ran.Next(-15, 15);
+                Console.Write("{0} ", array[i]);
+
+            }
+            int[] result = arrswitch1.ArrSwitch(array);
+            Console.WriteLine();
+
+            foreach (int i in result)
+
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab4/ConsoleApp7/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Switch
+    {
+        public int[] ArrSwitch(int[] mass)
+        {
+            int imin = mass.Select((v, i) => v == mass.Min() ? i + 1 : 0).OrderBy(v => v).Where(v => v != 0).First() - 1; //индекс первого минимального
+            int imax = mass.Length - mass.Reverse().Select((v, i) => v == mass.Max() ? i + 1 : 0).OrderBy(v => v).Where(v => v != 0).First();//индекс последнего максимального
+            int imin1 = mass.Select((v, i) => v == mass.Max() ? i + 1 : 0).OrderBy(v => v).Where(v => v != 0).First() - 1; //индекс первого минимального
+            int imax1 = mass.Length - mass.Reverse().Select((v, i) => v == mass.Min() ? i + 1 : 0).OrderBy(v => v).Where(v => v != 0).First();//индекс последнего максимального
+            if (imin < imax)
+            {
+                int[] rev = mass.Skip(imin + 1).Take(imax - imin - 1).Reverse().ToArray();//выделяем нужные элементы и инвертируем
+                int[] before = mass.Where((v, i) => i <= imin).ToArray();//элементы до минимального (включая минимальный)
+                int[] after = mass.Where((v, i) => i >= imax).ToArray();//элементы после максимального (включая максимальный)
+                mass = before.Concat(rev).ToArray().Concat(after).ToArray();//объединяем
+
+            }
+            if (imin1 < imax1)
+            {
+                int[] rev = mass.Skip(imin1 + 1).Take(imax1 - imin1 - 1).Reverse().ToArray();//выделяем нужные элементы и инвертируем
+                int[] before = mass.Where((v, i) => i <= imin1).ToArray();//элементы до минимального (включая минимальный)
+                int[] after = mass.Where((v, i) => i >= imax1).ToArray();//элементы после максимального (включая максимальный)
+                mass = before.Concat(rev).ToArray().Concat(after).ToArray();//объединяем
+
+            }
+
+            return mass;
+        }
+    }
+}
