f0269fc0417ffc465138a1260fcb9b688f411578
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-05-07 13:24:59 +0600

    Add files via upload

---
+++ lab7/ConsoleApp4/Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace lab_7
+{
+    public class Matrix
+    {
+
+        public (int,int) Operation(int[,] matrix)
+        {
+
+            int count = 0;
+            int sum = 0;
+            int mid = 0;
+            
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum =+ matrix[i,j];
+                    
+                }
+            }
+            mid = sum / matrix.GetLength(0);
+
+            int lowerSum = 0;
+            for (int a = 0; a < matrix.GetLength(0); a++)
+            {
+                for (int b = 0; b < matrix.GetLength(1); b++)
+                {
+
+                    if (b > a && matrix[a, b] > mid)
+                    {
+                        count++;
+                    }
+                    if (b < a && matrix[a, b] > mid)
+                    {
+                        lowerSum += matrix[a, b];
+                    }
+                }
+            }
+                                  
+
+            return (count, lowerSum);
+        }
+        
+
+    }
+}
---
+++ lab7/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace lab_7
+{
+    public class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[,] matrix = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(100);
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Matrix newmatrix = new Matrix();
+            (int count, int sum) = newmatrix.Operation(matrix);
+            Console.Write("{0} ", count);
+            Console.Write("{0} ", sum);
+            Console.ReadLine();
+
+        }
+    }
+}
+
