21ab60d9936f650ffa1ccd5c95dc231c28a74492
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-05-07 13:24:33 +0600

    Add files via upload

---
+++ lab6/ConsoleApp5/DeleteFromArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab6
+{
+    public class DeleteFromArray
+    {
+
+        public List<int> DeleteElement(List<int> array)
+        {
+            int a = Math.Abs(array[0]);
+
+            for (int i = 0; i < array.Count; i++)
+            {
+
+                if (array[i] % a == 0)
+                {
+                    array.RemoveAt(i);
+                }
+            }
+
+            return array;
+        }
+
+
+    }
+}
