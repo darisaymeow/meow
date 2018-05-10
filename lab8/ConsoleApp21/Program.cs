477e5bf9a4fb1ddbb3e5c181c057fdf0832782c1
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-05-11 02:10:54 +0600

    Add files via upload

---
+++ lab8/ConsoleApp21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DZ_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите количество столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Zeroer zeroer = new Zeroer();
+            List<int> EmptyRow = zeroer.MakeZeroColumn(rows);
+            List<List<int>> list = zeroer.MakeRandomList(rows, columns);
+
+            zeroer.OutputList(rows, list);
+
+            double average = zeroer.CalcAvgOfPositive(list);
+            if (average != 0)
+            {
+                Console.WriteLine("Средние значение положительных элементов равно = " + average);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].Exists(value => Math.Abs(value) > average))
+                    {
+                        list.Insert(i + 1, EmptyRow);
+                    }
+                }
+                Console.WriteLine("Выводим новый массив");
+                zeroer.OutputList(rows, list);
+            }
+            else
+            {
+                Console.WriteLine("Нет положительных элементов.");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+
+    }
+}
+
---
+++ lab8/ConsoleApp21/Zeroer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DZ_8
+{
+    class Zeroer
+
+    {
+        public void OutputList(int rows, List<List<int>> list)
+        {
+            for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < list.Count; columnIndex++)
+                {
+                    Console.Write("{0,3} ", list[columnIndex][rowIndex].ToString());
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+        public double CalcAvgOfPositive(List<List<int>> columns)
+        {
+            return columns.SelectMany(row => row.FindAll(value => value > 0)).DefaultIfEmpty(0).Average();
+        }
+        public List<List<int>> MakeRandomList(int rows, int columns)
+        {
+            List<List<int>> Array = new List<List<int>>();
+            Random rnd = new Random();
+            for (int i = 0; i < columns; i++)
+            {
+                List<int> ArrayRow = new List<int>();
+                for (int j = 0; j < rows; j++)
+                {
+                    ArrayRow.Add(rnd.Next(-10, 10));
+                }
+                Array.Add(ArrayRow);
+            }
+            return Array;
+        }
+        public List<int> MakeZeroColumn(int rows)
+        {
+            List<int> EmptyRow = new List<int>();
+            for (int j = 0; j < rows; j++)
+            {
+                EmptyRow.Add(0);
+            }
+            return EmptyRow;
+        }
+    }
+}
