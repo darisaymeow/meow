6cb2daf63d5970239481f32d6185a5463227926b
Daria <36181776+darisaymeow@users.noreply.github.com>
2018-05-14 21:17:15 +0600

    Add files via upload

---
+++ lab1/ConsoleApp23/DaysСountering.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class DaysСountering 
+    {
+        public int countering(int year)
+        {
+            int days = 0;
+
+            if ((year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+
+            {
+                days = 366;
+            }
+            else
+            {
+                days = 365;
+            }
+
+            return days;
+        }
+        
+    }
+}
---
+++ lab1/ConsoleApp23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 0;
+            int days = 0;
+            Console.WriteLine("Введите год: ");
+            year = int.Parse(Console.ReadLine());
+            DaysСountering counter = new DaysСountering();
+            days = counter.countering(year);
+            Console.WriteLine(days);
+            Console.Read();
+        }
+    }
+}
